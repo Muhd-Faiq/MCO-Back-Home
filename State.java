a5a547fae7708627eb64f1f7b479005d3a27cbe1
Muhammad Faiq Bin Roslan <mfaiqroslan@gmail.com>
2020-07-18 08:51:48 +0800

    Create State.java

---
+++ State.java
@@ -0,0 +1,99 @@
+
+
+
+class State
+{
+	private String state1;
+	public State(){};
+	public String getStateex()
+	{
+		return state1;
+	}
+	public void setStateex(int cho)
+	{
+		if(cho==1)
+		{
+			state1="Perlis";
+		}
+		else if(cho==2)
+		{
+			state1="Kelantan";
+		}
+		else if(cho==3)
+		{
+			state1="Kedah";
+		}
+		else if(cho==4)
+		{
+			state1="Pulau Pinang";
+		}
+		else if(cho==5)
+		{
+			state1="Terengganu";
+		}
+		else if(cho==6)
+		{
+			state1="Perak";
+		}
+		else if(cho==7)
+		{
+			state1="Selangor";
+		}
+		else if(cho==8)
+		{
+			state1="Pahang";
+		}
+		else if(cho==9)
+		{
+			state1="Negeri Sembilan";
+		}
+		else if(cho==10)
+		{
+			state1="Melaka";
+		}
+		else if(cho==11)
+		{
+			state1="Johor";
+		}
+		else if(cho==12)
+		{
+			state1="Sarawak";
+		}
+		else if(cho==13)
+		{
+			state1="Sabah";
+		}
+		else if(cho==14)
+		{
+			state1="WP Kuala Lumpur";
+		}
+		else if(cho==15)
+		{
+			state1="WP Putrajaya";
+		}
+		else if(cho==16)
+		{
+			state1="WP Labuan";
+		}
+		else
+		{
+			state1=null;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
